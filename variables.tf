f4519bbbf8444612e235fc70a6ccf99d1a57a2a3
qatip <michael2697@hotmail.com>
2026-01-09 01:50:53 +0000

    Publish network module v1.0.0

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "location" {}
+variable "prefix" {}
+variable "resource_group_name" {}
+variable "base_tags" {}
+variable "nsg_id" {}
+
+
+variable "vnet_address_space" {
+  type        = string
+  description = "CIDR for the Virtual Network address space."
+}
+
+variable "subnet_cidrs" {
+  type        = map(string)
+  description = "Map of logical subnet name to CIDR."
+
+  validation {
+    condition = alltrue([
+      for cidr in values(var.subnet_cidrs) :
+      can(cidrhost(trimspace(cidr), 0))
+    ])
+    error_message = "All subnet_cidrs values must be valid."
+  }
+}
